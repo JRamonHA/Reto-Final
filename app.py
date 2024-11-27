f61d59062e589d02501442d0a774768fa84ef05d
JRamonHA <hdezagjr@gmail.com>
2024-11-26 18:54:27 -0600

    First commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from shiny import render
+from shiny.express import input, ui
+import pandas as pd
+import matplotlib.pyplot as plt
+import funciones as fu
+
+f = fu.cargar_anio("2010")
+esol = pd.read_parquet(f)
+nombres = list(esol)
+
+with ui.sidebar(open="closed", bg="#f8f8f8"):  
+    ui.input_select("year", "Año", list(fu.years)) 
+    ui.input_select("columna", "Variable", nombres)
+    ui.input_checkbox("maximos", "Resample max")  
+
+@render.plot() 
+def plot_columna():  
+    year_selected = input.year()
+    f = fu.cargar_anio(year_selected)
+    esol = pd.read_parquet(f)
+
+    fig, ax = plt.subplots()
+    ax.plot(esol[input.columna()])
+    if input.maximos():
+        ax.plot(esol[input.columna()].resample("D").max(), color="red")
+
+    return fig
---
+++ funciones.py
@@ -0,0 +1,9 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read("years.ini")
+years = config.sections()
+
+def cargar_anio(year):
+    year_config = config[year]
+    return year_config['f']
